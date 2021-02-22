0b12dc583e038b056c870001b1544f1e4ee2cda1
Brice-Tokouete <bricetok@gmail.com>
2021-02-22 16:41:35 -0600

    First commit - contact class

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+    def __init__(self, id, lastname, firstname, contactDetails,creationDate):
+        self.id = id
+        self.lastname = lastname
+        self.firstname = firstname
+        self.contactDetails = contactDetails
+        self.creationDate = creationDate
