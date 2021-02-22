ffa92d8d08c423e0e0929f3b86f8502c6df4bec3
Brice-Tokouete <bricetok@gmail.com>
2021-02-22 17:27:26 -0600

    First Commit - Import of library, query and read execution function

---
+++ contactstable.py
@@ -0,0 +1,44 @@
+# import of contact class
+# import of datetime library and mysql connector
+from contact import Contact
+import datetime
+from datetime import date
+import mysql.connector
+from mysql.connector import Error
+
+# defined the creation of the connection to the database from inclass example
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+# defined the query and read execution function from inclass example
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
